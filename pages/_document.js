de23deb0ccb1e76046727242fa8291f2a6d1b7d4
santillienzo <enzo135246@gmail.com>
2022-06-27 18:23:21 -0300

    feat: instalar material-ui y chakra ui

---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+
+class MyDocument extends Document {
+    render() {
+        return (
+        <Html>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                />
+
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/icon-500x500.png" />
+                
+                <meta name='theme-color' content='#1a1a1a'/>
+
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+                
+            </body>
+        </Html>
+        )
+    }
+}
+
+export default MyDocument
