9bb38efa6510279cde2f668dd95ea7efeb31585c
santillienzo <enzo135246@gmail.com>
2022-06-27 19:38:00 -0300

    feat: desintalar chakra

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import '../styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+        <Component {...pageProps} />
   )
 }
 
